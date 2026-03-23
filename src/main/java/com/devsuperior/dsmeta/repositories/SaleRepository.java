ac9a432c4c6fbfec642b736402009a836874655c
Toshiaki Okayama Filho <okayamafilho@gmail.com>
2026-03-23 15:09:14 -0300

    implementação das querys no SaleRepository

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,43 @@
 package com.devsuperior.dsmeta.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
+import java.util.List;
 
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.dto.SaleSumDTO;
 import com.devsuperior.dsmeta.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SaleMinDTO(obj.id, obj.amount, obj.seller.name) "
+          + "FROM Sale obj "
+          + "WHERE obj.date BETWEEN :minDate AND :maxDate " 
+          + "AND LOWER(obj.seller.name) LIKE LOWER(CONCAT('%', :name,  '%'))")
+    Page<SaleMinDTO> searchByFilters(
+        @Param("minDate") LocalDate minDate, 
+        @Param("maxDate") LocalDate maxDate,
+        @Param("name") String name, 
+        Pageable pageable         
+    );         
+
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SaleSumDTO(obj.seller.name, SUM(obj.amount)) " 
+          + "FROM Sale obj " 
+          + "WHERE obj.date BETWEEN :minDate AND :maxDate " 
+          + "AND LOWER(obj.seller.name) LIKE LOWER(CONCAT('%', :name, '%')) "
+          + "GROUP BY obj.seller.name")
+    List<SaleSumDTO> summaryByFilters(
+        @Param("minDate") LocalDate minDate, 
+        @Param("maxDate") LocalDate maxDate, 
+        @Param("name") String name
+    );          
+           
+
+
 
 }
